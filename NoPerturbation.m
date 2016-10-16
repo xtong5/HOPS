4ff7aca1cab4669bef97be2acb8287754a5f1597
Xin Tong <xtong5@hawk.iit.edu>
2016-10-16 13:21:23 -0500

    data

--- NoPerturbation.m
+++ NoPerturbation.m
@@ -117,34 +117,40 @@ W_norm(m) = norm(W_exact(:,m),2)/sqrt(N_theta);
 
 end
 
-lambda_crit_plot = lambda_crit*ones(M,1);
-norm_max = max([max(U_norm),max(W_norm),max(BU_norm)]);
-yy = linspace(0,norm_max,M)';
+filename = sprintf('data_%s.mat','NoPer');
+save(filename,'M','lambda','U_norm','W_norm','BU_norm',...
+    'lambda_crit','epsilon_u_plot','epsilon_w_plot','Eps','a','b',...
+    'N_theta','N')
 
-figure(1);
-plot(lambda,U_norm,'b-o',lambda,W_norm,'g-*',...
-    lambda,BU_norm,'c-d',lambda_crit_plot,yy,'r--');
-xlabel('$\lambda$','interpreter','latex');
-ylabel('$|U|_2$ and $|W|_2$','interpreter','latex');
-title('$|U|_2$ and $|W|_2$ versus $\lambda$','interpreter','latex');
-legend('|U|_2','|W|_2','|BU|_2','lambda_c');
+% lambda_crit_plot = lambda_crit*ones(M,1);
+% norm_max = max([max(U_norm),max(W_norm),max(BU_norm)]);
+% yy = linspace(0,norm_max,M)';
 
+
+% figure(1);
+% plot(lambda,U_norm,'b-o',lambda,W_norm,'g-*',...
+%     lambda,BU_norm,'c-d',lambda_crit_plot,yy,'r--');
+% xlabel('$\lambda$','interpreter','latex');
+% ylabel('$|U|_2$ and $|W|_2$','interpreter','latex');
+% title('$|U|_2$ and $|W|_2$ versus $\lambda$','interpreter','latex');
+% legend('|U|_2','|W|_2','|BU|_2','lambda_c');
+% 
 
 
 %
 % Plot of critical lambda
 %
 
-eps_max = max(max(epsilon_u_plot),max(-real(epsilon_w_plot)));
-yy = linspace(0,eps_max,M)';
-
-figure(2);
-plot(lambda,epsilon_u_plot,'b-o',lambda,-real(epsilon_w_plot),'g-*',...
-    lambda_crit_plot,yy,'r--');
-xlabel('$\lambda$','interpreter','latex');
-ylabel('$\epsilon$','interpreter','latex');
-title('$\epsilon_u$ and -Real($\epsilon_w$) versus $\lambda$','interpreter','latex');
-legend('epsilon_u','-Re[epsilon_w]','lambda_c');
+% eps_max = max(max(epsilon_u_plot),max(-real(epsilon_w_plot)));
+% yy = linspace(0,eps_max,M)';
+% 
+% figure(2);
+% plot(lambda,epsilon_u_plot,'b-o',lambda,-real(epsilon_w_plot),'g-*',...
+%     lambda_crit_plot,yy,'r--');
+% xlabel('$\lambda$','interpreter','latex');
+% ylabel('$\epsilon$','interpreter','latex');
+% title('$\epsilon_u$ and -Real($\epsilon_w$) versus $\lambda$','interpreter','latex');
+% legend('epsilon_u','-Re[epsilon_w]','lambda_c');
 
  
 
