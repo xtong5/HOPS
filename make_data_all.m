d953cfcafbf6d848ab7e9611352d9f6bbbd742ef
Xin Tong <xtong5@hawk.iit.edu>
2016-10-22 16:32:30 -0500

    data pade

--- make_data_all.m
+++ make_data_all.m
@@ -5,38 +5,40 @@ a = 0.025;
 b = 10*a;
 N = 16;
 % Eps = 0.01*a; %0.1*a
-Eps = 0.1*a;
+Eps = 0.01*a;
 M = 401;
+
+warning('off');
 
 theta = (L/N_theta)*[0:N_theta-1]';
 
 f = exp(cos(theta));
-name = 'expcos10';
-FE_application(M,f,N_theta,a,b,N,Eps,L,theta,name);
+name = 'expcos100pade';
+FE_applicationPade(M,f,N_theta,a,b,N,Eps,L,theta,name);
 
 
 f = cos(2*theta);
-name = 'cos210';
-FE_application(M,f,N_theta,a,b,N,Eps,L,theta,name);
+name = 'cos2100pade';
+FE_applicationPade(M,f,N_theta,a,b,N,Eps,L,theta,name);
 
 f = cos(4*theta);
-name = 'cos410';
-FE_application(M,f,N_theta,a,b,N,Eps,L,theta,name);
+name = 'cos4100pade';
+FE_applicationPade(M,f,N_theta,a,b,N,Eps,L,theta,name);
 
 f = cos(8*theta);
-name = 'cos810';
-FE_application(M,f,N_theta,a,b,N,Eps,L,theta,name);
+name = 'cos8100pade';
+FE_applicationPade(M,f,N_theta,a,b,N,Eps,L,theta,name);
 
 
-ff = zeros(N_theta/4,1);
-for i=0:N_theta/8
-    ff(i+1) = a/cos(theta(i+1));
-end
-for i=N_theta/8+1:N_theta/8*2-1
-    ff(i+1) = a/sin(theta(i+1));
-end
-f = [ff; ff; ff; ff];
-name = 'square10';
-FE_application(M,f,N_theta,a,b,N,Eps,L,theta,name);
+% ff = zeros(N_theta/4,1);
+% for i=0:N_theta/8
+%     ff(i+1) = a/cos(theta(i+1));
+% end
+% for i=N_theta/8+1:N_theta/8*2-1
+%     ff(i+1) = a/sin(theta(i+1));
+% end
+% f = [ff; ff; ff; ff];
+% name = 'squareEps1pade';
+% FE_applicationPade(M,f,N_theta,a,b,N,1,L,theta,name);
 
 
